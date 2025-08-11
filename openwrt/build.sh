7092aad1e3105f81a5b7f716e5d165238d4d8cd7
MrZhuQiu <84578919+MrZhuQiu@users.noreply.github.com>
2025-08-11 14:20:32 +0800

    Update build.sh

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -37,9 +37,9 @@ ip_info=`curl -sk https://ip.cooluc.com`;
 
 # script url
 if [ "$isCN" = "CN" ]; then
-    export mirror=https://init.cooluc.com
+    export mirror=https://proxy.gitwarp.com/https://github.com/MrZhuQiu/r4s_build_script/blob/master/build.sh
 else
-    export mirror=https://init2.cooluc.com
+    export mirror=https://proxy.gitwarp.com/https://github.com/MrZhuQiu/r4s_build_script/blob/master/build.sh
 fi
 
 # github actions - caddy server
