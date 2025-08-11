c744505250e195c8dff3873bab620a51c0bdcff6
MrZhuQiu <84578919+MrZhuQiu@users.noreply.github.com>
2025-08-11 12:11:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-openwrt/build.sh
+ # script url
+ if [ "$isCN" = "CN" ]; then
++    export mirror=https://raw.githubusercontent.com/MrZhuQiu/r4s_build_script/refs/heads/master
+ else
++    export mirror=https://raw.githubusercontent.com/MrZhuQiu/r4s_build_script/refs/heads/master
+ fi
