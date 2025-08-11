6e558a8f8da77d5ec1e4691ce2a86e5760712696
MrZhuQiu <84578919+MrZhuQiu@users.noreply.github.com>
2025-08-11 14:20:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,1 @@
- # script url
- if [ "$isCN" = "CN" ]; then
-+    export mirror=https://proxy.gitwarp.com/https://github.com/MrZhuQiu/r4s_build_script/blob/master/build.sh
- else
-+    export mirror=https://proxy.gitwarp.com/https://github.com/MrZhuQiu/r4s_build_script/blob/master/build.sh
- fi
+openwrt/build.sh
