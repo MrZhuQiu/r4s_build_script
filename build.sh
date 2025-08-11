6ac2d42a26690e30b436c20c590f946b45350abe
MrZhuQiu <84578919+MrZhuQiu@users.noreply.github.com>
2025-08-11 12:19:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
  # script url
  if [ "$isCN" = "CN" ]; then
-+    export mirror=https://raw.githubusercontent.com/MrZhuQiu/r4s_build_script/refs/heads/master
++    export mirror=https://proxy.gitwarp.com/https://github.com/MrZhuQiu/r4s_build_script/blob/master/build.sh
  else
-+    export mirror=https://raw.githubusercontent.com/MrZhuQiu/r4s_build_script/refs/heads/master
++    export mirror=https://proxy.gitwarp.com/https://github.com/MrZhuQiu/r4s_build_script/blob/master/build.sh
  fi
